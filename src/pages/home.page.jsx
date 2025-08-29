65130c23ec49582725537687b9db5866d7c2209b
kazirobin <kazirobinking@gmail.com>
2025-08-29 23:28:10 +0600

    start

---
+++ src/pages/home.page.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <>
+    <h1 className="text-black">I don't care bro</h1>
+    </>
+  }
+}
+
+export default Home;
