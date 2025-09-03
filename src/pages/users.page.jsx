c1b0fe79ba1f69bc600b3e141b66e000f9a6ffcb
kazirobin <kazirobinking@gmail.com>
2025-09-03 06:48:31 +0600

    4 page component created

--- src/pages/users.page.jsx
+++ src/pages/users.page.jsx
@@ -1,23 +1,45 @@
 import { Component } from "react";
-import Table from "../components/table.component";
+import Table from "./../components/table.component";
+import { Link } from "react-router";
+import axios from "axios";
+import Row from "../components/row.component";
 
-class Products extends Component {
+class Users extends Component {
+  state = {
+    data: [],
+  };
+  componentDidMount() {
+    const promise = axios.get("https://dummyjson.com/products");
+    promise.then((res, req) => {
+      this.setState({ data: res.data.products });
+      console.log(this.state.data)
+    }).catch((error) => { console.log("Api Error :",error.message) })
+  }
   render() {
+    const products = this.state.data
     return (
-      <>
-         <li  className="relative group ">
-              <Link
-                to="/"
-                className="text-gray-300 hover:text-white font-medium transition duration-300 flex"
-              >
-                Go Home
-              </Link>
-              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
-            </li>
-        <Table />
-      </>
+      <div className="bg-[#0077b6]   mx-auto">
+        <div className="max-w-4xl mx-auto py-4">
+          <div className="relative inline py-2 group  mx-auto">
+            <Link
+              to="/"
+              className="text-gray-300 hover:text-white font-medium transition duration-300"
+            >
+              ◀ Go Back Home
+              <span className="absolute left-0 -bottom-1 w-0 h-0.5 bg-orange-400 transition-all duration-300 group-hover:w-full"></span>
+            </Link>
+          </div>
+        </div>
+
+        <Table title={"Users"}  columnA={"Title"} columnB={"category"} columnC={"Price"} columnD={"rating"} img={"Images"}>
+          {products.map((product) => (
+
+      <Row key={product.id} columnA={product.title} columnB={product.category} columnC={product.price} columnD={product.rating} imgSrc={product.images[0]}/>
+          ))}
+        </Table>
+      </div>
     );
   }
 }
 
-export default Products;
+export default Users;
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import Home from "../pages/home.page";
 import Error from "../pages/error.page";
 import Products from "../pages/products.page";
+import Carts from "../pages/carts.page";
+import Recipes from "../pages/recipes.page";
+import Users from "../pages/users.page";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +15,21 @@ export const router = createBrowserRouter([
   {
     path: "/products",
     element: <Products/>,
+    errorElement: <Error />,
+  },
+  {
+    path: "/carts",
+    element: <Carts/>,
+    errorElement: <Error />,
+  },
+  {
+    path: "/recipes",
+    element: <Recipes/>,
+    errorElement: <Error />,
+  },
+  {
+    path: "/users",
+    element: <Users/>,
     errorElement: <Error />,
   },
 ]);
